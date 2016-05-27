a4b0c6cd3d4e27ba51817845baee2f944d1e6f78
Rahmat Budiharso <rbudiharso@gmail.com>
2016-05-27 17:27:35 +0700

    add bang

--- server.go
+++ server.go
@@ -13,10 +13,15 @@ func pong(c *gin.Context) {
 	c.JSON(200, gin.H{"message": "PONG"})
 }
 
+func bang(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "BANG BANG"})
+}
+
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	server := gin.New()
 	server.GET("/", ping)
 	server.GET("/pong", pong)
+	server.GET("/bang", bang)
 	server.Run()
 }
