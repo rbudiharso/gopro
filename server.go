dea6cad14f2af14736928f2a862590ba4670678e
Rahmat Budiharso <rbudiharso@gmail.com>
2016-05-27 17:24:50 +0700

    add pong

--- server.go
+++ server.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"runtime"
-
 	"github.com/gin-gonic/gin"
+	"runtime"
 )
 
 func ping(c *gin.Context) {
 	c.JSON(200, gin.H{"message": "OK"})
 }
 
+func pong(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "PONG"})
+}
+
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	server := gin.New()
 	server.GET("/", ping)
-	server.GET("/ping", ping)
+	server.GET("/pong", pong)
 	server.Run()
 }
